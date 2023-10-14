c50f9734a35e76851126e8df52dee6819ecdb99f
Amir <93887854+any-questions@users.noreply.github.com>
2023-10-14 04:21:59 +0300

    Delete Lab_1/Lab_1_task_3 directory

--- Lab_1/Lab_1_task_3/Lab_1_task_3.py
+++
@@ -1,6 +0,0 @@
-list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-
-# TODO Разделите участников на две команды
-number_of_players = len(set(list_players))
-print(list_players[0:number_of_players//2])
-print(list_players[number_of_players//2:number_of_players])
